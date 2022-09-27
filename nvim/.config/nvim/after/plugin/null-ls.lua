83eeba1a884208c1fb55eab76d8bc1343161b599
Johannes Klint (bia342) <johannes.klint@teliacompany.com>
2022-09-27 16:04:17 +0200

    Add format on save

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -40,6 +40,16 @@ local function on_attach(client, _)
   if client.resolved_capabilities.document_formatting then
     vim.cmd("command! -buffer Formatting lua vim.lsp.buf.formatting()")
     vim.cmd("command! -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
+
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        vim.lsp.buf.formatting_sync()
+      end,
+    })
   end
 end
 
