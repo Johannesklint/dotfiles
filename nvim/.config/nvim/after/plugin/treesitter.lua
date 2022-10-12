214267e70e9c85e5ff8da4b2e944b1739976a1de
Johannes Klint (bia342) <johannes.klint@teliacompany.com>
2022-10-12 10:35:02 +0200

    Add correct syntax hightlighting config

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -2,11 +2,32 @@ local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup({
   playground = { enable = true },
-  highlight = { enable = true },
-  indent = { enable = true },
+  highlight = { 
+    enable = true,
+    disable = {}
+  },
+  indent = {
+    enable = true, 
+    disable = {} 
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua"
+  },
   autotag = { enable = true }, 
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
   }
 })
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+
