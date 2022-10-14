330140917e95ab64731d76bc9981c0df0835697d
Johannes Klint (bia342) <johannes.klint@teliacompany.com>
2022-10-14 13:45:42 +0200

    Change telescope config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,25 +1,32 @@
 local actions = require('telescope.actions')
 
 require("telescope").setup {
-  pickers = {
-    find_files = {
-      theme = "ivy",
-    },
-    buffers= {
-      theme = "ivy",
-    },
-    live_grep= {
-      theme = "ivy",
-    },
-    lsp_references= {
-      theme = "ivy",
-    },
-  },
   defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      vertical = {
+        width = 0.95,  
+        height = 0.95, 
+      },
+    },
     mappings = {
       n = {
         ["q"] = actions.close
       },
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    buffers= {
+      theme = "dropdown",
+    },
+    live_grep= {
+      theme = "dropdown",
+    },
+    lsp_references= {
+      theme = "dropdown",
     },
   },
 }
