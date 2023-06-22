1d972abb43397270eb54d8ff0635d2d9910d15f8
Johannes Klint <johannes.klint@teliacompany.com>
2023-06-22 13:21:14 +0200

    Change telesope display

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local actions = require('telescope.actions')
 
 require("telescope").setup {
   defaults = {
-    -- path_display = { "tail" },
+    path_display = { "truncate" },
     layout_strategy = "horizontal",
     layout_config = {
         width = 0.95,  
